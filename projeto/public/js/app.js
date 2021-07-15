c24aaed57c81feb71d012fc39b9a76393a75b844
DannyCastroVentura <dannycastroventura@gmail.com>
2021-07-15 13:03:59 +0100

    change app.js constant

--- projeto/public/js/app.js
+++ projeto/public/js/app.js
@@ -1,6 +1,9 @@
 (() => {
     'use strict';
 
+    const locationHost = location.host.split(":");
+    sessionStorage.setItem("E-Teacher Location Host", locationHost[0]);
+    console.log(sessionStorage.getItem("E-Teacher Location Host"));
 
     let fetching = false;
 
@@ -17,7 +20,7 @@
         .service('AllOrientacoesService', AllOrientacoesService)
         .service('AllRecursosDigitaisService', AllRecursosDigitaisService)
         .service('AllLogsService', AllLogsService)
-        .constant('ApiBasePath', "http://locahost:8080/FinalProject")
+        .constant('ApiBasePath', "http://" + sessionStorage.getItem("E-Teacher Location Host") + ":8080/FinalProject")
         .directive('loading', ['$http', function ($http) {
             return {
                 restrict: 'A',
